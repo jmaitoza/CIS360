5214a15711d851c23b8dac0103a4344d8dafe347
Joshua Maitoza <60358495+jmaitoza@users.noreply.github.com>
2022-11-21 12:44:12 -0500

    welp part 3 works

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -9,37 +9,37 @@ bool ratioCompare(pair <int, int> a, pair <int, int> b) {
 }
 
 
-void DP01Knapsack(int n, int W, int Profits[n], int Weights[n])
-{
-    int includedProfits = 0;
-    int omittedProfits = 0;
-    //initialization
-    // P (n+1) x (W+1) matrix
-    int P[n+1][W+1];
-
-    for (int w = 0 ; w <= W ; w++)
-        P[0][w] = 0;
-    for (int i = 0 ; i <= n ; i++)
-        P[i][0] = 0;
-
-    // Profit calculation
-    for (int i = 0; i < n; i++)
-    {
-        for (int w = 0; w < W; w++)
-        {
-            if (Weights[i] <= w)
-            {
-                includedProfits = Profits[i] + P[i][w-Weights[i]];
-                omittedProfits = P[i][w];
-                P[i+1][w] = max(includedProfits, omittedProfits);
-            }
-            else
-                P[i+1][w] = P[i][w];
-        }
-    }
-    cout << "Included profit: " << includedProfits << endl;
-    cout << "Omitted profit: " << omittedProfits << endl;
-}
+//void DP01Knapsack(int n, int W, int Profits[n], int Weights[n])
+//{
+//    int includedProfits = 0;
+//    int omittedProfits = 0;
+//    //initialization
+//    // P (n+1) x (W+1) matrix
+//    int P[n+1][W+1];
+//
+//    for (int w = 0 ; w <= W ; w++)
+//        P[0][w] = 0;
+//    for (int i = 0 ; i <= n ; i++)
+//        P[i][0] = 0;
+//
+//    // Profit calculation
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int w = 0; w < W; w++)
+//        {
+//            if (Weights[i] <= w)
+//            {
+//                includedProfits = Profits[i] + P[i][w-Weights[i]];
+//                omittedProfits = P[i][w];
+//                P[i+1][w] = max(includedProfits, omittedProfits);
+//            }
+//            else
+//                P[i+1][w] = P[i][w];
+//        }
+//    }
+//    cout << "Included profit: " << includedProfits << endl;
+//    cout << "Omitted profit: " << omittedProfits << endl;
+//}
 
 void fracKnapsack(int n, int W, vector<int> profits, vector<int> weights)
 {
